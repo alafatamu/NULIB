b6e5ca5d35cd738d65b33e674683bb2f68de53aa
alafa <alex_afa21@tamu.edu>
2026-04-22 06:54:00 -0500

    Added histos.cpp and histos.hpp. Haven't done anything with them yet.

--- src/analysis/histos.cpp
+++ src/analysis/histos.cpp
@@ -1,3 +1,17 @@
+#include <iostream>
+#include <vector>
+
 #include "analysis/histos.hpp"
 
 #include "TFile.h"
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+void base_histos(TFile& ROOTFile){ //open a root file and make histograms based on the "raw" tree
+  TFile HDump("histos.root","UPDATE");
+  if(HDump.IsZombie()){
+    std::cout<<"Error: could not open histos.root"<<std::endl;
+    return;
+  }
+
+}
